d65fb2f6c8ff6c349c89e5ad1c9b644b75e9124f
Adrián Justino Maestre <ajustino22@ilg.cat>
2022-11-12 17:27:43 +0100

    Commit para desahcer

--- Program.java
+++ Program.java
@@ -1,14 +1,15 @@
 /*
- * Program.java        10.10.2022
+ * Program.java        20.10.2020
  *
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2022 Justino <ajustino22@ilg.cat>
+ * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
  */
+
 
 import java.util.Scanner;
 
