3b009f215b8144a671b2510645bfe95c0cbcc958
Adrián Justino Maestre <ajustino22@ilg.cat>
2022-11-12 16:55:01 +0100

    Primer commit

---
+++ Program.java
@@ -0,0 +1,57 @@
+/*
+ * Program.java        10.10.2022
+ *
+ * Program to calculate the area and the perimeter
+ * of a circle knowing the radius in meter.
+ *
+ * Copyright 2022 Justino <ajustino22@ilg.cat>
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See http://www.gnu.org/licenses/gpl.html for more information.
+ */
+
+import java.util.Scanner;
+
+public class Program {
+	/**
+	 * Calculates the area of a circle
+	 * 
+	 * @param radius is a double numbers
+	 * @return a double: the area of a circle
+	 */
+	public static double area(double radius) {
+
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+	/**
+	 * Calculates the perimeter of a circle
+	 * 
+	 * @param radius is a double numbers
+	 * @return a double: the perimeter of a circle
+	 */
+	public static double perimeter(double radius) {
+
+		return (2 * Math.PI) * radius;
+	}
+
+	/**
+	 * TUI = Terminal User Interface
+	 * 
+	 * @param args not used.
+	 */
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter the radius: ");
+		double radi = in.nextDouble();
+
+		in.close();
+
+		System.out.println("\nThe area is: " + area(radi));
+
+		System.out.println("\nThe perimeter is: " + perimeter(radi));
+
+	}
+}
